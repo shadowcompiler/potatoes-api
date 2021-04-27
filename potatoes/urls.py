f9a4b19332966fc339bdc6bad986aa237fd5b701
henri-dev <pariso03henri@gmail.com>
2021-04-27 13:41:05 +0100

    - add router

--- potatoes/urls.py
+++ potatoes/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from potatoes import views
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewset, basename='hello-viewset')
 
 urlpatterns = [
     path('hello-view/', views.HelloWorld.as_view()),
+    path('', include(router.urls)),
 ]
